40b79b44d11e1960382d0e8c8aba379a22b3e894
Dudko Kate <ekaterina_dr@tut.by>
2021-11-07 11:21:59 +0300

    added function initCarousel

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
-}
+let offsetWidth = document.querySelector('.carousel__slide').offsetWidth;
+let carusel = document.querySelector('.carousel__inner');
+let elements = document.querySelectorAll('.carousel__slide');
+let rightButton =document.querySelector('.carousel__arrow_right');
+let leftButton =document.querySelector('.carousel__arrow_left');
+
+let position = 0;
+let index = 1;
+
+rightButton.onclick = function() {
+  position = -(offsetWidth * index);
+  carusel.style.transform = `translateX(${position}px)`;
+  index++;
+  checkButton();
+   };
+
+  leftButton.onclick = function() {
+  position += offsetWidth;
+  carusel.style.transform = `translateX(${position}px)`;
+  index--;
+  
+  checkButton();
+ };
+
+
+ function checkButton() {
+  rightButton.style.display = index === elements.length ? 'none' : '';
+  leftButton.style.display = index === 1 ? 'none' : '';
+ }
+ 
+ checkButton();
+};
