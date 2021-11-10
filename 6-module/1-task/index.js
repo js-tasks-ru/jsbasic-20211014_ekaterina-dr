d90115948db1dd27530f37572d65681b91525934
Dudko Kate <ekaterina_dr@tut.by>
2021-11-10 16:24:01 +0300

    added class UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,47 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = document.createElement('table');
+    this.createTable();
   }
+
+  createTable() {
+    let thead = document.createElement('thead');
+    thead.innerHTML = ` <tr>
+    <th>name</th>
+    <th>age</th>
+    <th>salary</th>
+    <th>city</th>
+    <th></th>
+    </tr>`;
+
+    this.elem.appendChild(thead);
+
+    let tbody = document.createElement('tbody');
+
+    this.rows.forEach(row => {
+      let tr = document.createElement('tr');
+      tr.innerHTML = `<td>${row.name}</td>
+     <td>${row.age}</td>  
+     <td>${row.salary}</td>
+     <td>${row.city}</td>`;
+
+      let td = document.createElement('td');
+      let button = document.createElement('button');
+      button.onclick = this.onclick;
+      button.innerHTML = 'X';
+
+      td.appendChild(button);
+      tr.appendChild(td);
+      tbody.appendChild(tr);
+    })
+
+    this.elem.appendChild(tbody);
+  }
+
+  onclick(event) {
+    event.target.closest('tr').remove();
+  }
+
 }
